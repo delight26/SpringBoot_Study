d7ac3a03d7e5e61651ddf40e8d38ad8d1c56fb76
이준범 <@DESKTOP-86Q8G1B>
2020-05-31 13:40:44 +0900

    2020.05.31 Rest API RestTemplate config 파일 설정 방법 UriComponentsBuilder 활용 방법 TestCode 작성 방법 리스트로 응답 방법 예제 스터디

---
+++ Ch05/src/test/java/info/thecodinglive/RestAPITests.java
@@ -0,0 +1,24 @@
+package info.thecodinglive;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import info.thecodinglive.model.User;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = RestApp.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class RestAPITests {
+	@Autowired
+	private TestRestTemplate restTemplate;
+	
+	@Test
+	public void testGetUserById() {
+		String url = "http://localhost:8080/user/0";
+		User user = restTemplate.getForObject(url, User.class);
+		System.out.println("µÓ∑œ¿œ : " + user.getRegDate() + " ," + user.getUserId() + " ," + user.getUname());
+	}
+}
